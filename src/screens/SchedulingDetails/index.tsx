de3f08329bf4fb55e7c35f3cdea2803e5b984d4a
Givaldo Souza <givaribeiro23@gmail.com>
2022-01-19 16:55:24 -0300

    import  Components Styles as S

--- src/screens/SchedulingDetails/index.tsx
+++ src/screens/SchedulingDetails/index.tsx
@@ -14,102 +14,78 @@ import GasolineSvg from '../../assets/gasoline.svg';
 import ExchangeSvg from '../../assets/exchange.svg';
 import PeopleSvg from '../../assets/people.svg';
 
-import { 
-  Container,
-  Header,
-  CarImages,
-  Content,
-  Details,
-  Description,
-  Brand,
-  Name,
-  Rent,
-  Period,
-  Price,
-  RentalPeriod,
-  CalendarIcon,
-  Accessories,
-  Footer,
-  DateInfo,
-  DateTitle,
-  DateValue,
-  RentalPrice,
-  RentalPriceLabel,
-  RentalPriceDetails,
-  RentalPriceQuota,
-  RentalPriceTotal,
- } from './styles';
+import  * as S from './styles';
 import { Button } from '../../components/Button';
 
 
 export function SchedulingDetails(){ 
   const theme = useTheme()
   return (
-    <Container>     
-      <Header>
+    <S.Container>     
+      <S.Header>
         <BackButton />
-      </Header>
-      <CarImages>
+      </S.Header>
+      <S.CarImages>
         <ImageSlider 
           imageUri={['https://media.gazetadopovo.com.br/2020/01/17155825/lamborghini-huracan-Alexander-Migl-wikimedia-commons.jpg']}
         />
-      </CarImages>
+      </S.CarImages>
 
-      <Content>
-        <Details>
-          <Description>
-            <Brand>Audi</Brand>
-            <Name>R8</Name>
-          </Description>
-          <Rent>
-            <Period>Ao dia</Period>
-            <Price>R$ 560</Price>
-          </Rent>
-        </Details>
-        <Accessories>
+      <S.Content>
+        <S.Details>
+          <S.Description>
+            <S.Brand>Audi</S.Brand>
+            <S.Name>R8</S.Name>
+          </S.Description>
+          <S.Rent>
+            <S.Period>Ao dia</S.Period>
+            <S.Price>R$ 560</S.Price>
+          </S.Rent>
+        </S.Details>
+        <S.Accessories>
           <Accessory icon={SpeedSvg} name='256km/h'/>
           <Accessory icon={ForceSvg} name='sf'/>
           <Accessory icon={AccelerationSvg} name='1.5s'/>
           <Accessory icon={GasolineSvg} name='sf'/>
           <Accessory icon={ExchangeSvg} name='sf'/>
           <Accessory icon={PeopleSvg} name='2'/>
-        </Accessories>
+        </S.Accessories>
 
         
-        <RentalPeriod>
-          <CalendarIcon>
+        <S.RentalPeriod>
+          <S.CalendarIcon>
             <Feather 
               name="calendar"
               size={RFValue(24)}
               color={theme.colors.shape}
             />
-          </CalendarIcon>
-          <DateInfo>
-            <DateTitle>DE</DateTitle>
-            <DateValue>19/01/2022</DateValue>
-          </DateInfo>
+          </S.CalendarIcon>
+          <S.DateInfo>
+            <S.DateTitle>DE</S.DateTitle>
+            <S.DateValue>19/01/2022</S.DateValue>
+          </S.DateInfo>
             <Feather 
               name="chevron-right"
               size={RFValue(15)}
               color={theme.colors.text}
             />
-          <DateInfo>
-            <DateTitle>ATÉ</DateTitle>
-            <DateValue>19/01/2022</DateValue>
-          </DateInfo>
-        </RentalPeriod>
+          <S.DateInfo>
+            <S.DateTitle>ATÉ</S.DateTitle>
+            <S.DateValue>19/01/2022</S.DateValue>
+          </S.DateInfo>
+        </S.RentalPeriod>
 
-        <RentalPrice>
-          <RentalPriceLabel>TOTAL</RentalPriceLabel>
-          <RentalPriceDetails>
-            <RentalPriceQuota>R$ 580 x3</RentalPriceQuota>
-            <RentalPriceTotal>R$ 2.900,00</RentalPriceTotal>
-          </RentalPriceDetails>
-        </RentalPrice>
-      </Content>
-      <Footer>
+        <S.RentalPrice>
+          <S.RentalPriceLabel>TOTAL</S.RentalPriceLabel>
+          <S.RentalPriceDetails>
+            <S.RentalPriceQuota>R$ 580 x3</S.RentalPriceQuota>
+            <S.RentalPriceTotal>R$ 2.900,00</S.RentalPriceTotal>
+          </S.RentalPriceDetails>
+        </S.RentalPrice>
+      </S.Content>
+      <S.Footer>
         <Button title='Alugar agora' color={theme.colors.success}/>
-      </Footer>
-    </Container>
+      </S.Footer>
+    </S.Container>
   );
 }
