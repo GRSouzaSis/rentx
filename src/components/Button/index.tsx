c6f94fe932b307677dcfdd782ea9ef3bc17cdfea
Givaldo Souza <givaribeiro23@gmail.com>
2022-01-19 16:09:43 -0300

    add: lib calendars, component Calendar init

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { TouchableOpacityProps } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
 import { useTheme } from 'styled-components';
 
 import { 
   Container,
   Title
- } from './styles';
- 
+ } from './styles'; 
  interface Props extends TouchableOpacityProps {
    title: string;
    color?: string;
