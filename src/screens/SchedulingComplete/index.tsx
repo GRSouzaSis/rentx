013e7c22ef5749f0fa3c2c0c755a81d94a43d73a
Givaldo Souza <givaribeiro23@gmail.com>
2022-01-20 09:50:11 -0300

    add: screen SchedulingComplete

---
+++ src/screens/SchedulingComplete/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StatusBar, useWindowDimensions } from 'react-native';
+import { useTheme } from 'styled-components';
+import LogoSvg from '../../assets/logo_background_gray.svg';
+import DoneSvg from '../../assets/done.svg';
+import * as S from './styles';
+import { Button } from '../../components/Button';
+
+export function SchedulingComplete(){ 
+  const { width } = useWindowDimensions()
+  const theme = useTheme()
+  return (
+    <S.Container>
+       <StatusBar 
+        barStyle='light-content'
+        backgroundColor="transparent"
+        translucent
+      />
+      <LogoSvg width={width}/>
+      <S.Content>
+        <DoneSvg />
+        <S.Title>Carro alugado!</S.Title>
+        <S.Message>
+          {`Agora você só precisa ir\n até a concessionária da RENTX\n pegar o seu automóvel.`}
+        </S.Message>
+        <S.ContentButton>
+         <Button 
+          title='OK' 
+          color={theme.colors.shape_dark}
+        />
+        </S.ContentButton>
+      </S.Content>
+    </S.Container>
+  );
+}
